681cb8ed273abd6204bff51f4e68d7c4186b1ffe
Felix Arntz <felixarntz@google.com>
2019-09-22 22:51:09 +0200

    Re-introduce sidebar.

--- inc/Sidebars/Component.php
+++ inc/Sidebars/Component.php
@@ -27,6 +27,8 @@ use function dynamic_sidebar;
  */
 class Component implements Component_Interface, Templating_Component_Interface {
 
+	const PRIMARY_SIDEBAR_SLUG = 'primary';
+
 	/**
 	 * Gets the unique identifier for the theme component.
 	 *
@@ -40,7 +42,8 @@ class Component implements Component_Interface, Templating_Component_Interface {
 	 * Adds the action and filter hooks to integrate with WordPress.
 	 */
 	public function initialize() {
-		// Empty method body.
+		add_action( 'widgets_init', [ $this, 'action_register_sidebars' ] );
+		add_filter( 'body_class', [ $this, 'filter_body_classes' ] );
 	}
 
 	/**
@@ -58,18 +61,52 @@ class Component implements Component_Interface, Templating_Component_Interface {
 	}
 
 	/**
+	 * Registers the sidebars.
+	 */
+	public function action_register_sidebars() {
+		register_sidebar(
+			[
+				'name'          => esc_html__( 'Sidebar', 'wp-rig' ),
+				'id'            => static::PRIMARY_SIDEBAR_SLUG,
+				'description'   => esc_html__( 'Add widgets here.', 'wp-rig' ),
+				'before_widget' => '<section id="%1$s" class="widget %2$s">',
+				'after_widget'  => '</section>',
+				'before_title'  => '<h2 class="widget-title">',
+				'after_title'   => '</h2>',
+			]
+		);
+	}
+	/**
+	 * Adds custom classes to indicate whether a sidebar is present to the array of body classes.
+	 *
+	 * @param array $classes Classes for the body element.
+	 * @return array Filtered body classes.
+	 */
+	public function filter_body_classes( array $classes ) : array {
+		if ( $this->is_primary_sidebar_active() ) {
+			global $template;
+
+			if ( ! in_array( basename( $template ), [ '404.php', '500.php', 'offline.php' ] ) ) {
+				$classes[] = 'has-sidebar';
+			}
+		}
+
+		return $classes;
+	}
+
+	/**
 	 * Checks whether the primary sidebar is active.
 	 *
 	 * @return bool True if the primary sidebar is active, false otherwise.
 	 */
 	public function is_primary_sidebar_active() : bool {
-		return false;
+		return (bool) is_active_sidebar( static::PRIMARY_SIDEBAR_SLUG );
 	}
 
 	/**
 	 * Displays the primary sidebar.
 	 */
 	public function display_primary_sidebar() {
-		// Empty method body.
+		dynamic_sidebar( static::PRIMARY_SIDEBAR_SLUG );
 	}
 }
